1e91cbd98a1c1a5eea0993364881bdd2f303d3fb
caoyzh <caoyazhen_ok@163.com>
2020-03-16 15:17:35 +0800

    Update the define of UNLIKELY/LIKELY.

--- lib/common/compiler.h
+++ lib/common/compiler.h
@@ -152,8 +152,8 @@
  * and clang, please do.
  */
 #if defined(__GNUC__)
-#define LIKELY(x) (__builtin_expect((x), 1))
-#define UNLIKELY(x) (__builtin_expect((x), 0))
+#define LIKELY(x) (__builtin_expect(!!(x), 1))
+#define UNLIKELY(x) (__builtin_expect(!!(x), 0))
 #else
 #define LIKELY(x) (x)
 #define UNLIKELY(x) (x)
